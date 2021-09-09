29bf09ec00de518fa1c321a5a9264c9918d17f38
Wendson <wendsonsantanajps@gmail.com>
2021-09-09 16:58:08 -0300

    Transformam variables

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 print('Hello Word')
+
+x = 10
+
+print(x)
+print(type(x))
+
+z = 3.545
+print('TYPE FLOAT')
+print(z)
+print(type(z))
+
+print('bolean')
+
+bolean = True
+
+print(bolean)
+print(type(bolean))
+
+print('String')
+
+text = 'String'
+
+print(text)
+print(type(text))
+
+print('Transformações')
+
+floatParaInt = int (5.24)
+intParaString = str(floatParaInt)
+strParaFloat = float (intParaString)
+
+print(floatParaInt)
+print(type(floatParaInt))
+print(intParaString)
+print(type(intParaString))
+print(strParaFloat)
+print(type(strParaFloat))
