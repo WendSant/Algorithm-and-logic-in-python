31dae0ea6cbdf97001a47ea29c020205a7d28962
Wendson <wendsonsantanajps@gmail.com>
2021-09-09 17:21:32 -0300

    Operadores

---
+++ operadores.py
@@ -0,0 +1,20 @@
+text1 = 'eu'
+text2 = 'tu'
+
+print(text1<text2);
+
+v = True
+f = False
+print("Operadores boleanos")
+print('Op. Logico v and f :', v and f)
+print('Op. Logico v or f :', v or f)
+print('Op. Logico v and not f :', v and not f)
+
+# Expoente unários
+
+print(-2**2)
+print(2**-2)
+
+# Operador logico
+
+print(False != 3 > 2 and True)
