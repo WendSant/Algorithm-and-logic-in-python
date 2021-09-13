7bfd980bfb0e8a87b7782914a97d04980d592a05
Wendson <wendsonsantanajps@gmail.com>
2021-09-12 21:49:45 -0300

    subtraction

--- somaUser.py
+++ somaUser.py
@@ -1,4 +1,9 @@
+# numero1 = int (input("Digite o primeiro numero: "))
+# numero2 = int (input("Digite o segundo numero: "))
+#
+# print('A soma dos número é: ', numero1+numero2)
+
 numero1 = int (input("Digite o primeiro numero: "))
 numero2 = int (input("Digite o segundo numero: "))
 
-print('A soma dos número é: ', numero1+numero2)
+print('A subtração dos números é: ', numero1-numero2)
