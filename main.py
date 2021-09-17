00b5d29ef8baa11da0977329a8734ac251c417b8
Wendson <wendsonsantanajps@gmail.com>
2021-09-16 23:57:18 -0300

    Exercise college

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-print('Hello Word')
+# print('Hello Word')
+#
+# x = 10
+#
+# print(x)
+# print(type(x))
+#
+# z = 3.545
+# print('TYPE FLOAT')
+# print(z)
+# print(type(z))
+#
+# print('bolean')
+#
+# bolean = True
+#
+# print(bolean)
+# print(type(bolean))
+#
+# print('String')
+#
+# text = 'String'
+#
+# print(text)
+# print(type(text))
+#
+# print('Transformações')
+#
+# floatParaInt = int (5.24)
+# intParaString = str(floatParaInt)
+# strParaFloat = float (intParaString)
+#
+# print(floatParaInt)
+# print(type(floatParaInt))
+# print(intParaString)
+# print(type(intParaString))
+# print(strParaFloat)
+# print(type(strParaFloat))
 
-x = 10
+s = 0
+i = 0
+n = 10
+while i<n:
+    if i>n// 3:
+        s+=i
+        i+= 1
+    else:
+        print(i,s)
 
-print(x)
-print(type(x))
-
-z = 3.545
-print('TYPE FLOAT')
-print(z)
-print(type(z))
-
-print('bolean')
-
-bolean = True
-
-print(bolean)
-print(type(bolean))
-
-print('String')
-
-text = 'String'
-
-print(text)
-print(type(text))
-
-print('Transformações')
-
-floatParaInt = int (5.24)
-intParaString = str(floatParaInt)
-strParaFloat = float (intParaString)
-
-print(floatParaInt)
-print(type(floatParaInt))
-print(intParaString)
-print(type(intParaString))
-print(strParaFloat)
-print(type(strParaFloat))
